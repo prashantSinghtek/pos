2281c3e2bd5ed58cfdae3978679eb2f7700446f6
RichaGupta07 <richa.g@singhsoft.com>
2024-09-17 14:00:25 +0530

    Add Redux Logger so that we can find out the which slice is calling

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 // store/index.ts
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger'; // Import redux-logger
 import rootReducer from './rootReducer';
 import { rootSaga } from './RootSaga';
 
@@ -9,8 +10,9 @@ const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware, logger), // Add redux-logger here
 });
 
 sagaMiddleware.run(rootSaga);
+
 export type RootState = ReturnType<typeof store.getState>;
